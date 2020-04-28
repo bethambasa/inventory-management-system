6ae805d86014f833bc00c57545fc7b7e4b9f3316
Abigael Ambasa <bethabby5@gmail.com>
2020-04-28 11:59:03 -0700

    fix

--- config/config.py
+++ config/config.py
@@ -14,4 +14,5 @@ class Development(config):
 class Production(config):
     SQLALCHEMY_DATABASE_URI='postgres://zqgtjktttrhkai:084fd839350946db1d97269c132c0f717a5dc954b6380c3172d407387de6cc0e@ec2-52-201-55-4.compute-1.amazonaws.com:5432/d5agpncjn4ork6'
     SECRET_KEY='secretcode'
-        pass
+
+    
